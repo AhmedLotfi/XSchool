02b6096b72fbdeb500821fb7061ac2975aba3ddb
ahmedlotfiali <ahmedlotfiali@gmail.com>
2021-05-28 16:19:44 +0400

    add basic components & ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccountsComponent } from './pages/accounts/accounts.component';
+import { UsersComponent } from './pages/users/users.component';
+import { NavBarComponent } from './pages/core/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent,
+    UsersComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
